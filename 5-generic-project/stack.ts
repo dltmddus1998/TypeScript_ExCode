642c8605f4589854e6b9f17667e65a245a85f741
dltmddus1998 <dltmddus1998@naver.com>
2022-05-03 22:46:29 +0900

    ADD: 5. generic example codes

--- 5-generic-project/stack.ts
+++ 5-generic-project/stack.ts
@@ -1,18 +1,18 @@
-interface Stack {
+interface Stack<T> {
     readonly size: number;
-    push(value: string): void;
-    pop(): string;
+    push(value: T): void;
+    pop(): T;
 }
 
-type StackNode = {
-    readonly value: string;
-    readonly next?: StackNode; 
+type StackNode<T> = {
+    readonly value: T;
+    readonly next?: StackNode<T>; 
 }
 
-class StackImpl implements Stack {
+class StackImpl<T> implements Stack<T> {
     private _size: number = 0;
     
-    private head?: StackNode;
+    private head?: StackNode<T>;
 
     constructor(private capacity: number) {}
 
@@ -20,11 +20,12 @@ class StackImpl implements Stack {
         return this._size;
     }
     
-    push(value: string) {
+    push(value: T) {
         if(this._size === this.capacity) {
             throw new Error('Stack is full!');
         }
-        const node: StackNode = {
+        // head에 이미 정확한 타입이 명시되어 있는 상황 -> 제네릭 타입 생략 가능 (타입 추론)
+        const node = {
             value,
             next: this.head
         };
@@ -32,7 +33,7 @@ class StackImpl implements Stack {
         this._size++;
     }
 
-    pop(): string {
+    pop(): T {
         if (this.head == null) { 
             throw new Error("Stack is Empty");
         }
@@ -43,10 +44,18 @@ class StackImpl implements Stack {
     }
 }
 
-const stack = new StackImpl(10);
+const stack = new StackImpl<string>(10);
 stack.push('sy 1');
 stack.push('bob 2');
 stack.push('steve 3');
 while(stack.size !== 0) {
     console.log(stack.pop());
+}
+
+const stack2 = new StackImpl<number>(10);
+stack2.push(123);
+stack2.push(456);
+stack2.push(789);
+while(stack2.size !== 0) {
+    console.log(stack2.pop());
 }
